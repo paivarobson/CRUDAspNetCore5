1b96e1379c8d54b0c870e1db9817ed91f159e377
Robson Paiva <paivawork@hotmail.com>
2021-06-12 15:38:44 -0300

    Restore do arquivo launch.json e chamada da View Usuarios

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using CRUDAspNetCore5.Models;
+using System.Linq;
+
 namespace CRUDAspNetCore5.Controllers
 {
     public class HomeController : Controller
@@ -6,9 +9,22 @@ namespace CRUDAspNetCore5.Controllers
         public IActionResult Index()
         {
             return View();
-        }public IActionResult Cadastrar()
+        }
+        
+        public IActionResult Cadastrar(int? id)
         {
-            return View("FormUsuario");
+            if (id.HasValue && Usuario.Listagem.Any(u => u.Id == id))
+            {
+                var usuario = Usuario.Listagem.Single(u => u.Id == id);
+                return View(usuario);
+            }
+
+            return View();
+        }
+
+        public IActionResult Usuarios()
+        {
+            return View();
         }
     }
 }
